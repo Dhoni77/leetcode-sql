8d41f9cef4e23652bdfd0498425b27f3f1a2f58e
Dhoni77 <aldrinmarz7@gmail.com>
2022-01-20 22:22:44 +0530

    file added

---
+++ easy/biggest-single-number.sql
@@ -0,0 +1,31 @@
+-- Question 24
+-- Table my_numbers contains many numbers in column num including duplicated ones.
+-- Can you write a SQL query to find the biggest number, which only appears once.
+
+-- +---+
+-- |num|
+-- +---+
+-- | 8 |
+-- | 8 |
+-- | 3 |
+-- | 3 |
+-- | 1 |
+-- | 4 |
+-- | 5 |
+-- | 6 | 
+-- For the sample data above, your query should return the following result:
+-- +---+
+-- |num|
+-- +---+
+-- | 6 |
+-- Note:
+-- If there is no such number, just output null.
+
+INSERT INTO `jdbc`.`biggest-single-number`
+(`number`)
+VALUES
+(8), (8), (3), (3), (1), (4), (5), (6);
+
+-- solution
+
+select max(a.num) nums from ( select num, count(*) from biggestsinglenumber  group by num having count(*)=1)a;
